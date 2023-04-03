2a1730108e9fcd3e92d39b9b77984fb9798b3a5e
nkarl <21993921+nkarl@users.noreply.github.com>
2023-04-03 13:30:57 -0700

    Working on ui.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import AppUI from './components/app-ui.svelte';
+
+	let data = [];
+</script>
+
+<main>
+	<div style="text-align:center">
+		<h1>Welcome to My Yelp Business Data App!</h1>
+	</div>
+
+	<AppUI />
+
+	<footer>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	</footer>
+</main>
+
+<style>
+	main {
+		min-width: 100%;
+		min-height: 95vh;
+	}
+
+	footer {
+		position: absolute;
+		bottom: 0;
+		width: 100%;
+		height: 2.5rem;
+		text-align: center;
+		margin-top: 5%;
+		margin-bottom: 1%;
+	}
+</style>
---
+++ src/routes/components/app-ui.svelte
@@ -0,0 +1,171 @@
+<!-- App GUI -->
+<script>
+	import { onMount } from 'svelte';
+	import axios from 'axios';
+	// export let data = [{}, {}];
+
+	const endpoint_test = 'https://jsonplaceholder.typicode.com/posts';
+	const endpoint_req = 'http://localhost:3000/v1/states';
+	$: posts = [{}];
+	$: users = [{}];
+	$: states = [{}];
+    $: state = '';
+
+	const res = {};
+
+	async function test_endpoint() {
+		try {
+			/* default endpoint */
+			res.default = await axios.get(endpoint_test);
+			/*console.log(res.data);*/
+			posts = res.default.data;
+			users = res.default.data.map((entry) => {
+				entry.id, entry.title;
+			});
+		} catch (error) {
+			console.error(error);
+		} finally {
+			console.log('successful fetched data.');
+		}
+	}
+
+	async function getStates() {
+		try {
+			/* get states */
+			res.states = await axios.get(endpoint_req);
+			states = res.states.data;
+			console.log(states);
+		} catch (error) {
+			console.log('THERE IS AN ERROR.');
+			console.error(error);
+		} finally {
+			console.log('successful fetched data.');
+		}
+	}
+
+	async function getCities(state) {
+		try {
+			/* get states */
+			res.cities = await axios.get(endpoint_req + '?state=' + state);
+			states = res.cities.data;
+			console.log(states);
+		} catch (error) {
+			console.log('THERE IS AN ERROR.');
+			console.error(error);
+		} finally {
+			console.log('successful fetched data.');
+		}
+	}
+
+	onMount(async function () {
+		/*
+            // Default fetch API
+			const res = await fetch(endpoint);
+			const data = await res.json();
+			posts = data;
+        */
+        test_endpoint();
+		getStates();
+	});
+</script>
+
+<!-- App GUI -->
+<div class="app-container">
+	<!-- LEFT PANEL: CITY LIST PER SELECTED STATE -->
+	<div class="panel" id="left-panel">
+		<h3>State List DropDown</h3>
+		<select id="data-option-list" name="">
+			{#each states as s, index (index)}
+                <option id="s{index + 1}" value="" on:click={()=> state=s.state}>{JSON.stringify(s)}</option>
+			{/each}
+		</select>
+        <h3>City List</h3>
+        <select>
+            {#each }
+                <option value="">{}</option>
+            {/each}
+        </select>
+		<div class="card">
+			<!--{#each posts.slice(0, 5) as p}-->
+			<!--<p>{JSON.stringify(p.id)}: {JSON.stringify(p.title)}</p>-->
+			<!--{/each}-->
+		</div>
+	</div>
+
+	<!-- RIGHT PANEL: BUSINESS LIST PER SELECTED CITY -->
+	<div class="panel">
+		<h3>City List DropDown</h3>
+		<select id="data-option-list" name="">
+			{#each Array(5) as _, index (index)}
+				<option id="item0{index}" value="">City 0{index}</option>
+			{/each}
+		</select>
+		<h3>Business List</h3>
+		<div class="card">Card 4</div>
+	</div>
+</div>
+
+<!-- TEST DATA: first 5 items -->
+<div id="test-data" class="panel">
+	{#each posts.slice(0, 5) as article}
+		<div>
+			<p>{JSON.stringify(article)}</p>
+		</div>
+	{/each}
+</div>
+
+<!-- Style Sheet -->
+<style>
+	.app-container {
+		/*text-align: center;*/
+		width: 95%;
+		min-height: 95%;
+		display: flex;
+		margin-left: 2%;
+		margin-right: 2%;
+		font-size: 18px;
+	}
+
+	.panel {
+		border: 5px solid gray;
+		width: 1000px;
+		min-height: 95%;
+		margin: 5px 5px;
+		padding: 2% 2%;
+		flex-direction: row wrap;
+		border-radius: 25px;
+	}
+
+	.card {
+		width: 400px;
+		height: 200px;
+		border: 5px dotted red;
+		margin: 10px 10px;
+		padding: 10px 10px;
+		border-radius: 25px;
+	}
+
+	#data-option-list {
+		width: 90%;
+		height: 50px;
+		margin-left: 2%;
+		margin-right: 2%;
+		margin-bottom: 5%;
+		font-size: 1rm;
+	}
+
+	#left-panel {
+		width: unset;
+		width: 40%;
+	}
+
+	select {
+		border-radius: 10px;
+		font-size: 1em;
+	}
+
+	#test-data {
+		display: unset;
+		display: inline-block;
+	}
+</style>
